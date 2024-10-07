77367b89c57e654fd8dd74123a02e10843918b1c
Carlos Rocha <carlos.rocha01@gmail.com>
2024-10-07 11:59:42 -0300

    update

--- p1/scripts/server/entrypoint.sh
+++ p1/scripts/server/entrypoint.sh
@@ -8,10 +8,13 @@ sudo cp /home/vagrant/.ssh/iot_42 /vagrant_data/iot_42
 sudo cp /home/vagrant/.ssh/iot_42.pub /vagrant_data/iot_42.pub
 
 cat /home/vagrant/.ssh/iot_42.pub >> /home/vagrant/.ssh/authorized_keys
+
 sudo chown -R vagrant:vagrant /home/vagrant/.ssh
 
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -
+
 sudo ln -sf /usr/local/bin/kubectl /usr/bin/kubectl
+
 kubectl version --client
 
 echo "K3S Master instalado com sucesso!"
