3fe2dc51c0ba54f39c9592d8c95f9614d12cc6a2
Carlos Rocha <carlos.rocha01@gmail.com>
2024-10-07 11:27:45 -0300

    feat: implement part 1 of project

---
+++ p1/scripts/server/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo mkdir -p /home/vagrant/.ssh
+
+sudo ssh-keygen -t rsa -f /home/vagrant/.ssh/iot_42 -q -N ""
+
+sudo cp /home/vagrant/.ssh/iot_42 /vagrant_data/iot_42
+sudo cp /home/vagrant/.ssh/iot_42.pub /vagrant_data/iot_42.pub
+
+cat /home/vagrant/.ssh/iot_42.pub >> /home/vagrant/.ssh/authorized_keys
+sudo chown -R vagrant:vagrant /home/vagrant/.ssh
+
+curl -sfL https://get.k3s.io | sh -
+echo "K3S Master instalado com sucesso!"
